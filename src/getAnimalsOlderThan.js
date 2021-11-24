4433c87eee2f8d4c5acc6b56aeeb78489232e27f
Vitor Correa <vitorcorrea18@gmail.com>
2021-11-24 17:44:13 -0400

    concluido o requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,6 +2,9 @@ const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const animals = data.species.find((specie) => specie.name === animal);
+  const checkAge = animals.residents.every((resident) => resident.age > age);
+  return checkAge;
 }
 
 module.exports = getAnimalsOlderThan;
