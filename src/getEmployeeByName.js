4d0430cc44247c759cb1a6ff0959eb91f94cd124
Vitor Correa <vitorcorrea18@gmail.com>
2021-11-24 18:20:06 -0400

    concluido o requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,6 +2,10 @@ const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  let employeeFound = data.employees.find((employee) =>
+    employee.firstName === employeeName || employee.lastName === employeeName);
+  if (employeeFound === undefined) employeeFound = {};
+  return employeeFound;
 }
 
 module.exports = getEmployeeByName;
